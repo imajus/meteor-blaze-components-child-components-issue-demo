f6e247fb2b60e17d0809bbe2858120f5731128fc
Denis Perov <denis.perov@gmail.com>
2022-02-19 19:41:40 +0300

    Initial commit

---
+++ client/main.js
@@ -0,0 +1,9 @@
+import { BlazeComponent } from 'meteor/peerlibrary:blaze-components';
+
+class Template1 extends BlazeComponent {}
+class Template2 extends BlazeComponent {}
+class Template3 extends BlazeComponent {}
+
+Template1.register('template1');
+Template2.register('template2');
+Template3.register('template3');
